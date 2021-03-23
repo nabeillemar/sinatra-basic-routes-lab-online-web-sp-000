364395bb977875e3e2d1233aca791e1163844184
Nabeil <nabeillemar@gmail.com>
2021-03-22 22:10:37 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do 
+        "My name is Nabeil"
+    end 
+
+    get '/hometown' do 
+        "My hometown is Hayward"
+    end 
+
+    get '/favorite-song' do
+        "My favorite song is Mahroo"
+    end 
 end
